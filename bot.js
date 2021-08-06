51a69f0b32cd4bbbcc8b41c5d48d8f4c5e4354f9
itzJovens <82013409+itzJovens@users.noreply.github.com>
2021-08-05 21:10:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const opts = {
         password: 'oauth:u79zcmhtqum7o9we321k3xhpeh9x71'
     },
     channels: [
-        'itzjovens', 'uknwmyname', 'xdaboinextdoorx', 'karagii', 'sydeon'
+        'itzjovens', 'uknwmyname', 'xdaboinextdoorx', 'karagii', 'sydeon', 'srchfps'
     ]
 };
 
