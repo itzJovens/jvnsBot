1cc82b25fcabc9f3308596a06ca53b19d35843c3
itzJovens <82013409+itzJovens@users.noreply.github.com>
2021-08-26 18:51:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -232,7 +232,7 @@ client.on('message', (channel, tags, message, self) => {
   const isModUp = isBroadcaster || isMod;
   const isSubUp = isModUp || isSub;
   const isVIPUp = isSubUp || isVIP;
-  if (channel.includes('itzjovens')){
+  if (channel.includes('tsm_daequan')){
       if(isVIPUp || isStaff) return;
   if(message.toLowerCase().includes('!followage')) {
     client.say('followage_bot_', `!followage ${tags.username} ${channel.slice(1)}`);
